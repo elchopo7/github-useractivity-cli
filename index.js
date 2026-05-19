1853c1989780a32afffa10a89ae03b2c3a2dbb61
/elchopo7 <elchopo7@gmail.com>
2026-05-19 12:45:16 +0200

    index.js fetch function

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function getUsername() {
 
 const username = getUsername();
 console.log(`Fetching activity for ${username}...`);
+
+async function fecthActivity(username) {
+    const url = `https://api.github.com/users/${username}/events`;
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'User-Agent': 'github-activity-cli',
+                'Accept': 'application/vnd.github+json',
+            },
+        });
+
+        if (!response.ok) {
+            if(response.status === 404) {
+                console.error(`User "${username}" not found`);
+            } else {
+                console.error(`GitHub API request failed with status ${response.status}.`);
+            }
+            process.exit(1);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        console.error('Failed to fetch GitHub activity:', error.message);
+        process.exit(1);
+    }
+}
